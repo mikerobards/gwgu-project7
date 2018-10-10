329a5fb7a8e31118af81c72fc9684297562647c5
Michael Robards <mikerobards@icloud.com>
2018-10-10 08:36:48 -0400

    add type to list items

--- src/component/ListItem.js
+++ src/component/ListItem.js
@@ -6,8 +6,9 @@ export default class ListItem extends Component {
   render() {
     return (<li className="listItem"
       onClick={() => this.props.listItemClick(this.props)}>
-      <img src={this.props.categories[0].icon.prefix+"30"+this.props.categories[0].icon.suffix} alt={this.props.categories[0].name}/>
       {this.props.name}
+      <img src={this.props.categories[0].icon.prefix+"30"+this.props.categories[0].icon.suffix} alt={this.props.categories[0].name}/>
+      {this.props.categories[0].name}
     </li>);
   }
 }
